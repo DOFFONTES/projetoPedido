1d5d901872e51fffc2f1bd175dd100d90964b05e
David Fontes <doffontes@gmail.com>
2020-10-09 17:55:48 -0300

    Projeto criado

---
+++ src/test/java/com/davidFontes/ProjetoPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidFontes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
