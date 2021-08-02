6a402f81b475d098e571acad2d9daaac7f401815
David Fontes <doffontes@gmail.com>
2021-08-02 12:54:25 -0300

    Inserindo Pedido

--- src/main/java/com/davidFontes/recursos/PedidoRecurso.java
+++ src/main/java/com/davidFontes/recursos/PedidoRecurso.java
@@ -1,13 +1,22 @@
 package com.davidFontes.recursos;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.davidFontes.dominio.Categoria;
 import com.davidFontes.dominio.Pedido;
+import com.davidFontes.dto.CategoriaDTO;
 import com.davidFontes.servicos.PedidoServico;
 
 @RestController
@@ -22,5 +31,14 @@ public class PedidoRecurso {
 		Pedido obj = servico.buscar(id);
 		
 		return ResponseEntity.ok().body(obj);		
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj){
+		obj = servico.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/davidFontes/servicos/PedidoServico.java
+++ src/main/java/com/davidFontes/servicos/PedidoServico.java
@@ -1,11 +1,17 @@
 package com.davidFontes.servicos;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.davidFontes.dominio.ItemPedido;
+import com.davidFontes.dominio.PagamentoComBoleto;
 import com.davidFontes.dominio.Pedido;
+import com.davidFontes.dominio.enums.EstadoPagamento;
+import com.davidFontes.repositorios.ItemPedidoRepositorio;
+import com.davidFontes.repositorios.PagamentoRepositorio;
 import com.davidFontes.repositorios.PedidoRepositorio;
 import com.davidFontes.servicos.exception.ObjetoNaoEncontradoException;
 
@@ -15,11 +21,46 @@ public class PedidoServico {
 	@Autowired
 	PedidoRepositorio repo;
 	
+	@Autowired
+	private ProdutoServico produtoServico;
+	
+	@Autowired
+	private BoletoServico boletoServico;
+	
+	@Autowired
+	private PagamentoRepositorio pagamentoRepositorio;
+	
+	@Autowired
+	private ItemPedidoRepositorio itemPedidoRepositorio;
+	
 	public Pedido buscar(Integer id) {
 		 Optional<Pedido> obj = repo.findById(id);
 		 return obj.orElseThrow(() -> new ObjetoNaoEncontradoException(
 				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName())); 
-		 } 
+		 }
+	
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			
+			boletoServico.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepositorio.save(obj.getPagamento());
+		
+		for (ItemPedido ip: obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoServico.buscar(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepositorio.saveAll(obj.getItens());
+		return obj;
+	}
 
 
 
