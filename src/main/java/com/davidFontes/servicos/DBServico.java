4ce9b5e2524b23e57c0c8cfa09945c6e6628980c
David Fontes <doffontes@gmail.com>
2021-08-05 16:59:30 -0300

    Adicionando senha a Cliente

--- src/main/java/com/davidFontes/servicos/DBServico.java
+++ src/main/java/com/davidFontes/servicos/DBServico.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.davidFontes.dominio.Categoria;
@@ -32,7 +33,9 @@ import com.davidFontes.repositorios.ProdutoRepositorio;
 
 @Service
 public class DBServico {
-
+	
+	@Autowired
+	private BCryptPasswordEncoder pe;
 	@Autowired
 	private CategoriaRepositorio categoriaRepositorio;	
 	@Autowired
@@ -109,7 +112,7 @@ public class DBServico {
 		estadoRepositorio.saveAll(Arrays.asList(est1,est2));
 		cidadeRepositorio.saveAll(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@hotmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@hotmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "938338393"));
 		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
